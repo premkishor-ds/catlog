6993ea1ff5e481267df24e2bec96cca8a49b58f0
Prem Kishor - Dotsquares <102597355+premkishor-ds@users.noreply.github.com>
2025-12-26 14:44:22 +0530

    refix issue

--- next.config.js
+++ next.config.js
@@ -2,9 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  output: 'export',
   images: {
-    unoptimized: true,
     formats: ['image/avif', 'image/webp'],
     remotePatterns: [
       {
